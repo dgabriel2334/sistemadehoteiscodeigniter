a92758e4d26a017a74d4f88dde773d34364360f2
Dário Gabriel <dario.santos@westwing.com.br>
2023-04-25 17:36:57 -0300

    Redirect to login

--- application/views/client/client.php
+++ application/views/client/client.php
@@ -33,6 +33,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contatos</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Administrativo</a>
+                    </li>
                 </ul>
             </div>
         </div>
